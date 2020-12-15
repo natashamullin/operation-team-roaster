bf7081a3c7118360c794bd4a151f9580ab66315d
Natasha Mullin <69641107+natashamullin@users.noreply.github.com>
2020-12-14 23:15:42 -0800

    sample html added per requirements

--- src/template2.js
+++ src/template2.js
@@ -128,25 +128,9 @@ const generatePage = (team) => {
   </html>`
 };
 
-// const copyFile = () => {
-//     return new Promise((resolve, reject) => {
-//         fs.copyFile('./src/styles.css', './dist/styles.css', err => {
-//             // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
-//             if (err) {
-//                 reject(err);
-//                 // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
-//                 return;
-//             }
-//             // if everything went well, resolve the Promise and send the successful data to the `.then()` method
-//             resolve({
-//                 ok: true,
-//                 message: 'File created!'
-//             });
-//         });
-//     });
-// };
+
 module.exports = {
     generatePage,
     writeFile,
-    // copyFile
+
 };
